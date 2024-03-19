4606a015e06b5cd289dc83171ed26e1d8b77279e
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-03-19 18:57:10 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-center text-4xl font-bold text-indigo-600'>
+        Project setup
+      </h1>
+    </div>
+  );
+}
+
+export default App;
