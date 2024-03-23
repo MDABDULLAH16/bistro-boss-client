82dfb012b9e8325c91d5b125ef22865f949b05a7
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-03-23 17:16:16 +0600

    chef-service section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner/Banner";
 import Category from "./Cartegory/Category";
+import ChefService from "./ChefSecvice/ChefService";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <Category></Category>
+      <ChefService></ChefService>
     </div>
   );
 };
