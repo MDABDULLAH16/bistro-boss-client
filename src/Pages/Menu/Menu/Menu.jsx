dc1985901904a4355cd93ccc3b638b06a47c749b
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-03-24 16:22:21 +0600

    cover blur with react parallax

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Helmet} from "react-helmet-async";
-
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
+import PopularItem from '../../Home/PopularMenu/PopularItem';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h1>men men Menu </h1>
+            <Cover img={menuImg} title={'Our Menu'}></Cover>
+           
         </div>
     );
 };
