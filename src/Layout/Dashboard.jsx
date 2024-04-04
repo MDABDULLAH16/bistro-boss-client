76442c0cb47a5eea4601851a632f1ea5110222c6
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-04-05 00:47:09 +0600

    my cart item show and total calculate

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -3,67 +3,88 @@ import { FaShoppingCart, FaHome } from "react-icons/fa";
 import { FaCalendar } from "react-icons/fa6";
 import { IoIosWallet } from "react-icons/io";
 import { IoCalendarNumber } from "react-icons/io5";
+import { MdMenu, MdShop2 } from "react-icons/md";
 
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div className="flex">
+    <div className="flex m-0 ">
       <div className="w-64 min-h-screen bg-orange-400">
-        <ul>
+        <ul className="menu">
           <li>
             <NavLink
-              className="ml-4 my-4 flex items-center uppercase "
+              className="  my-2 flex items-center uppercase "
               to="/dashboard/userHome"
             >
-              <FaHome className="mr-3"></FaHome>User Home
+              <FaHome className="mr-3 text-2xl"></FaHome>User Home
             </NavLink>
           </li>
           <li>
             <NavLink
-              className="ml-4 my-4 flex items-center uppercase "
+              className="   my-2 flex items-center uppercase "
               to="/dashboard/reservation"
             >
-              <FaCalendar className="mr-3"></FaCalendar>reservation
+              <FaCalendar className="mr-3 text-2xl"></FaCalendar>reservation
             </NavLink>
           </li>
           <li>
             <NavLink
-              className="ml-4 my-4 flex items-center uppercase "
+              className="   my-2 flex items-center uppercase "
               to="/dashboard/paymentHistory"
             >
-              <IoIosWallet className="mr-3" />
+              <IoIosWallet className="mr-3 text-2xl" />
               payment history
             </NavLink>
           </li>
           <li>
             <NavLink
-              className="ml-4 my-4 flex items-center uppercase "
+              className="   my-2 flex items-center uppercase "
               to="/dashboard/cart"
             >
-              <FaShoppingCart className="mr-3"></FaShoppingCart>My Cart
+              <FaShoppingCart className="mr-3 text-2xl"></FaShoppingCart>My Cart
             </NavLink>
           </li>
           <li>
             <NavLink
-              className="ml-4 my-4 flex items-center uppercase "
+              className="   my-2 flex items-center uppercase "
               to="/dashboard/addReview"
             >
-              <FaShoppingCart className="mr-3"></FaShoppingCart>add review
+              <FaShoppingCart className="mr-3 text-2xl"></FaShoppingCart>add
+              review
             </NavLink>
           </li>
           <li>
             <NavLink
-              className="ml-4 my-4 flex items-center uppercase "
+              className="   my-2 flex items-center uppercase "
               to="/dashboard/myBooking"
             >
-              <IoCalendarNumber className="mr-3" />
+              <IoCalendarNumber className="mr-3 text-2xl" />
               my booking
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink className="   my-2 flex items-center uppercase " to="/">
+              <FaHome className="mr-3 text-2xl" />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="   my-2   flex items-center uppercase " to="/">
+              <MdMenu className="mr-3 text-2xl" />
+              menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="   my-2   flex items-center uppercase " to="/">
+              <MdShop2 className="mr-3 text-2xl" />
+              shop
             </NavLink>
           </li>
         </ul>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 min-h-screen bg-[#F6F6F6] p-0 m-0">
         <Outlet></Outlet>
       </div>
     </div>
--- src/Pages/Shared/MyCart/MyCart.jsx
+++ src/Pages/Shared/MyCart/MyCart.jsx
@@ -1,9 +1,77 @@
 import React from "react";
+import useCart from "../../../hooks/useCart/useCart";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { FaTrashAlt } from "react-icons/fa";
 
 const MyCart = () => {
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   return (
-    <div>
-      <h1>this is my Cart</h1>
+    <div className="   mt-0">
+      <SectionTitle
+        subHeading={"My-Cart"}
+        heading={"WANNA ADD MORE?"}
+      ></SectionTitle>
+      <div className="bg-white m-16 p-12">
+        <div className="flex justify-between mb-8 ">
+          <p className="text-3xl font-bold">Total Order:{cart.length}</p>
+          <p className="text-3xl font-bold">Total Price: {totalPrice}</p>
+          <button className="btn bg-[#D1A054] text-white btn-lg">Pay</button>
+        </div>
+        <div>
+          <div className="rounded overflow-hidden">
+            <table className="table text-lg">
+              {/* head */}
+              <thead className="">
+                <tr className="uppercase bg-[#D1A054] h-[72px] font-semibold text-white ">
+                  <th>
+                    <label>
+                      {/* <input type="checkbox" className="checkbox" /> */}
+                    </label>
+                  </th>
+                  <th>Item Image</th>
+                  <th>Item Name</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {cart.map((item, index) => (
+                  <tr key={item._id}>
+                    <th>
+                      <label>{index + 1}</label>
+                    </th>
+                    <td>
+                      <div className="flex items-center gap-3 ">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img
+                              src={item.image}
+                              alt="Avatar Tailwind CSS Component"
+                            />
+                          </div>
+                        </div>
+                        {/* <div>
+                        <div className="font-bold">{item.name}</div>
+                        <div className="text-sm opacity-50">United States</div>
+                      </div> */}
+                      </div>
+                    </td>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <th>
+                      <button className="btn btn-ghost btn-lg bg-red-400 text-white ">
+                        <FaTrashAlt className="text-2xl"></FaTrashAlt>{" "}
+                      </button>
+                    </th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
